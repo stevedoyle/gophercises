a07bfee82f062ad18a47d1adad602b0e2010e25a
Stephen Doyle <stephendoyle75@gmail.com>
2022-01-31 07:14:25 +0000

    Added quiz countdown timer

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 type Question struct {
@@ -13,14 +15,41 @@ type Question struct {
 }
 
 func main() {
+	// Flag processing
+	timeoutFlag := flag.Int("time", 30, "Quiz time limit")
+	flag.Parse()
+
+	correct := 0
+	incorrect := 0
+
+	fmt.Printf("Press Enter to start the Quiz ...")
+	var tmp string
+	fmt.Scanln(&tmp)
+	countdownTimer := time.NewTimer(
+		time.Duration(*timeoutFlag) * time.Second)
+
+	quizCh := make(chan int)
+	go do_quiz(&correct, &incorrect, quizCh)
+
+	select {
+	case <-countdownTimer.C:
+		fmt.Println("Quiz ountdown timer expired.")
+	case <-quizCh:
+		fmt.Println("Quiz completed!")
+	}
+
+	fmt.Printf("Correct: %d, Incorrect: %d\n", correct, incorrect)
+}
+
+func do_quiz(correct *int, incorrect *int, ch chan<- int) {
 	records, err := readData("questions.csv")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	incorrect := 0
-	correct := 0
+	*incorrect = 0
+	*correct = 0
 
 	for _, record := range records {
 		q := Question{
@@ -33,13 +62,12 @@ func main() {
 		var actual string
 		fmt.Scan(&actual)
 		if q.answer == actual {
-			correct += 1
+			*correct += 1
 		} else {
-			incorrect += 1
+			*incorrect += 1
 		}
 	}
-
-	fmt.Printf("Correct: %d, Incorrect: %d\n", correct, incorrect)
+	ch <- 1
 }
 
 func readData(fileName string) ([][]string, error) {
